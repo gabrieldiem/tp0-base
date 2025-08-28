cccdef2f53bee6a64ceb8c88a44442dee8d16799
gabrieldiem <gabriel.diem5@gmail.com>
2025-08-28 13:36:20 -0300

    feat-ej1: added docker compose generation script

---
+++ generar-compose.sh
@@ -0,0 +1,65 @@
+#!/usr/bin/env bash
+
+OUTPUT_FILENAME=$1
+NUM_CLIENTS=$2
+ERROR_EXIT_CODE=1
+
+# Check for both necessary inputs
+if [ -z "$OUTPUT_FILENAME" ] || [ -z "$NUM_CLIENTS" ]; then
+  echo "Usage: $0 <OUTPUT_FILENAME> <NUMBER_OF_CLIENTS>"
+  exit $ERROR_EXIT_CODE
+fi
+
+SERVER_SERVICE="
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+"
+
+# Write server to file as YAML, which will always be one even if there are n clients
+# Truncates (overwrites) if file already exists
+echo -e "name: tp0\nservices: $SERVER_SERVICE" > "$OUTPUT_FILENAME"
+
+# Returns the YAML string for a single client taking as first arg the client number
+format_client_service() {
+  local client_num="$1"
+  echo "
+  client$client_num:
+    container_name: client$client_num
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=$client_num
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+  "
+}
+
+# Append clients to YAML file
+for nth_client in $(seq 1 "$NUM_CLIENTS"); do
+  client_service=$(format_client_service $nth_client)
+  echo "$client_service" >> "$OUTPUT_FILENAME"
+done
+
+NETWORK="
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+"
+
+# Append the network info to YAML file
+echo " $NETWORK" >> "$OUTPUT_FILENAME"
+
+echo "Docker Compose YAML file generated at '$OUTPUT_FILENAME' with $NUM_CLIENTS clients"
